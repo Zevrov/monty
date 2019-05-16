26a7f985cfec7ade16218ee2ec4943389cf3771b
Wolf Hopkins <zevrov.719@gmail.com>
2019-05-16 14:14:35 -0400

    typo in params

--- create_buffer.c
+++ create_buffer.c
@@ -36,7 +36,7 @@ int create_buff(char *file_name)
 			continue;
 		}
 		argument_container.arguments = strtok(NULL, "\n\t ");
-		get_opcode(command, line, &stack);
+		get_opcode(&stack, line, command);
 		line++;
 	}
 	free_stack(&stack)
