8b45621943e04b4c48f395c824625f8fd80349ad
Wolf Hopkins <zevrov.719@gmail.com>
2019-05-16 18:23:06 -0400

    testing sub

--- sub.c
+++ sub.c
@@ -1,24 +1,23 @@
 #include "monty.h"
 /**
-* op_add - adds the top two elements of the stack,
+* op_sub - subtracts the top two elements of the stack,
 * and combines them into one node.
-* still working on edge cases
 * @stack: the memory
 * @line_number: the line
 */
-void op_add(stack_t **stack, unsigned int line_number)
+void op_sub(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp = NULL;
 	int math = 0;
 
 	if (!(*stack)->next || !*stack || !stack)
 	{
-		printf("L%d: can't add, stack too short\n", line_number);
+		printf("L%d: can't sub, stack too short\n", line_number);
 		free_stack(stack);
 		exit(EXIT_FAILURE);
 	}
 	temp = *stack;
-	math = temp->n + temp->next->n;
+	math = temp->n - temp->next->n;
 	op_pop(stack, line_number);
 	(*stack)->n = math;
 }
